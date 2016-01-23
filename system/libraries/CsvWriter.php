3db0a5504fc1110130137eeeb980319268e0382a
Leo Unglaub <leo@leo-unglaub.net>
2016-01-23 03:10:01 +0100

    Replace the copyright notice with a new short one
    Signed-off-by: Leo Unglaub <leo@leo-unglaub.net>

--- system/libraries/CsvWriter.php
+++ system/libraries/CsvWriter.php
@@ -1,31 +1,12 @@
 <?php
 
 /**
- * Contao Open Source CMS
- * Copyright (C) 2005-2011 Leo Feyer
+ * csv-writer
  *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  2create.at 2011
- * @author     Leo Unglaub <leo@leo-unglaub.net>
- * @package    CsvWriter
- * @license    LGPL
- * @filesource
+ * @copyright	Leo Unglaub 2016
+ * @author		Leo Unglaub <leo@leo-unglaub.net>
+ * @package		csv-writer
+ * @license		GPL
  */
 
 
